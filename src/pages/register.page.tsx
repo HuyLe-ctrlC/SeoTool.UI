4f8851f7965f86bce8c8b73e6b049c303a451893
Huy Le Minh <intern.leminhhuy@tma.com.vn>
2022-07-14 14:39:23 +0700

    commit

---
+++ src/pages/register.page.tsx
@@ -0,0 +1,103 @@
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import { object, string, TypeOf } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useRegisterUserMutation } from "../redux/api/authApi";
+import { toast } from "react-toastify";
+import { Button, Form } from "react-bootstrap";
+
+const registerSchema = object({
+  name: string().nonempty("Full name is required").max(100),
+  email: string()
+    .nonempty("Email address is required")
+    .email("Email Address is invalid"),
+  password: string()
+    .nonempty("Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+  passwordConfirm: string().nonempty("Please confirm your password"),
+}).refine((data) => data.password === data.passwordConfirm, {
+  path: ["passwordConfirm"],
+  message: "Passwords do not match",
+});
+
+export type RegisterInput = TypeOf<typeof registerSchema>;
+
+const RegisterPage = () => {
+  const methods = useForm<RegisterInput>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  //  Calling the Register Mutation
+  const [registerUser, { isLoading, isSuccess, error, isError }] =
+    useRegisterUserMutation();
+
+  const navigate = useNavigate();
+
+  const {
+    reset,
+    handleSubmit,
+    formState: { isSubmitSuccessful },
+  } = methods;
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("User registered successfully");
+      navigate("/verifyemail");
+    }
+
+    if (isError) {
+      console.log(error);
+      if (Array.isArray((error as any).data.error)) {
+        (error as any).data.error.forEach((el: any) =>
+          toast.error(el.message, {
+            position: "top-right",
+          })
+        );
+      } else {
+        toast.error((error as any).data.message, {
+          position: "top-right",
+        });
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading]);
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSubmitSuccessful]);
+
+  const onSubmitHandler: SubmitHandler<RegisterInput> = (values) => {
+    //  Executing the RegisterUser Mutation
+    registerUser(values);
+  };
+
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default RegisterPage;
